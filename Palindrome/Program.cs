7237718dcf175fd5a8b64b46b32991de04aa99e7
John Lundberg <john.joakim.lundberg@gmail.com>
2017-09-03 19:03:58 +0200

    All Files

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Palindrome
 {
@@ -10,6 +6,41 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            string Word;
+            string Reverse = "";
+            bool Forever = true;
+
+            while (Forever)
+            {
+                Console.WriteLine("Skriv in ditt ord för att kontrollera om det är en Palindrom");
+                Word = Console.ReadLine();
+
+                if (Word == "sluta")
+                {
+                    break;
+                }
+
+                for (int i = Word.Length - 1; i >= 0; i--)
+                {
+                    Reverse += Word[i].ToString();
+                }
+
+                if (Reverse == Word)
+                {
+                    Console.WriteLine("Strängen är en Palindrom \nDu skrev {0} och baklänges är det {1}", Word, Reverse);
+                }
+
+                else
+                {
+                    Console.WriteLine("Strängen är inte en Palindrom \nDu skrev {0} och baklänges är det {1}", Word, Reverse);
+                }
+
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            
         }
     }
 }
